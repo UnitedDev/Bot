595a198eedac60ac950d60e6dec4d3e4f0be3711
Hugo <58655174+Rhodless@users.noreply.github.com>
2022-07-19 14:11:19 +0200

    [/] Join fix 2

--- src/main/java/fr/kohei/bot/events/impl/GuildMemberJoinListener.java
+++ src/main/java/fr/kohei/bot/events/impl/GuildMemberJoinListener.java
@@ -12,7 +12,14 @@ public class GuildMemberJoinListener {
 
         embedBuilder.setThumbnail(event.getMember().getEffectiveAvatarUrl());
         embedBuilder.setTitle("Un nouveau membre !");
-        embedBuilder.setDescription("Bienvenue à **" + event.getMember().getEffectiveName() + "** sur Kohei. Pour suivre l'actualité du serveur rendez-vous sur le channel " + Main.getInstance().getGuild().getTextChannelById("992917852166307931").getAsMention() + ". Il y a maintenant un total de **" + Main.getInstance().getGuild().getMembers().size() + " **membres sur le discord.");
+        embedBuilder.setDescription("Bienvenue à **" + event.getMember().getEffectiveName() +
+                "** sur Kohei. Pour suivre l'actualité du serveur rendez-vous sur le channel " +
+                Main.getInstance()
+                        .getGuild()
+                        .getNewsChannelById("992917852166307931")
+                        .getAsMention() +
+                ". Il y a maintenant un total de **" + Main.getInstance().getGuild().getMembers().size() +
+                " **membres sur le discord.");
         embedBuilder.setFooter("Kohei'Bot", Main.getInstance().getGuild().getIconUrl());
         embedBuilder.setTimestamp(new Date().toInstant());
 
